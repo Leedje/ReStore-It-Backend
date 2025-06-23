5411ccaa467ab52f1decb0c2089c0ebb48351355
Leedje Bocio Amancio <545638@student.fontys.nl>
2025-06-23 02:54:51 +0200

    fixed order creation

--- ReStore-It/src/main/java/com/restoreit/services/OrderService.java
+++ ReStore-It/src/main/java/com/restoreit/services/OrderService.java
@@ -3,7 +3,9 @@ package com.restoreit.services;
 import com.restoreit.dtos.OrderDTO;
 import com.restoreit.mappers.OrderMapper;
 import com.restoreit.models.Order;
+import com.restoreit.models.Product;
 import com.restoreit.repository.OrderRepository;
+import com.restoreit.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,18 +17,27 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
     private final OrderMapper orderMapper;
     private final ChatRoomService chatRoomService;
 
     @Autowired
-    public OrderService(OrderRepository orderRepository, OrderMapper orderMapper, ChatRoomService chatRoomService) {
+    public OrderService(OrderRepository orderRepository, OrderMapper orderMapper, ChatRoomService chatRoomService, ProductRepository productRepository) {
         this.orderRepository = orderRepository;
+        this.productRepository = productRepository;
         this.orderMapper = orderMapper;
         this.chatRoomService = chatRoomService;
     }
 
     public UUID CreateOrder(OrderDTO orderDto) {
         Order order = orderMapper.DTOtoOrder(orderDto);
+
+        List<Product> attachedProducts = orderDto.products.stream()
+                .map(product -> productRepository.findById(product.id)
+                        .orElseThrow(() -> new RuntimeException("Product not found: " + product.id)))
+                .collect(Collectors.toList());
+
+        order.setProducts(attachedProducts);
         orderRepository.save(order);
 
         if(chatRoomService.CreateChatRooms(order)){
